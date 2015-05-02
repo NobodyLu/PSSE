c92739397800eae6b5b7d4fb6971904a0643f3e2
Longsky <hnlu@sjtu.edu.cn>
2015-05-02 17:55:17 +0800

    Finish (first step) implementation and test for document index operation.

--- PSSE/src/org/loccs/index/IndexQuerier.java
+++ PSSE/src/org/loccs/index/IndexQuerier.java
@@ -3,11 +3,24 @@ package org.loccs.index;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Vector;
-
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.MultiFields;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.PhraseQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
 
 public class IndexQuerier {
     private Analyzer analyzer;
@@ -22,9 +35,11 @@ public class IndexQuerier {
 //begin of modifiable zone(JavaSuper).....C/2b8973b1-fea5-4d1d-848b-aa84bc5ef60b
 
 //end of modifiable zone(JavaSuper).......E/2b8973b1-fea5-4d1d-848b-aa84bc5ef60b
-//begin of modifiable zone(JavaCode)......C/2b8973b1-fea5-4d1d-848b-aa84bc5ef60b
-    	fsDirectory = FSDirectory.open(Paths.get(directory));
-//end of modifiable zone(JavaCode)........E/2b8973b1-fea5-4d1d-848b-aa84bc5ef60b
+//begin of modifiable zone................T/e7a6a039-924a-4679-8c3b-5cb2c556b3c6
+        fsDirectory = FSDirectory.open(Paths.get(directory));
+        reader = DirectoryReader.open(fsDirectory);
+        searcher = new IndexSearcher(reader);
+//end of modifiable zone..................E/e7a6a039-924a-4679-8c3b-5cb2c556b3c6
     }
 
     void setAnalyzer(Analyzer value) {
@@ -34,40 +49,74 @@ public class IndexQuerier {
 //end of modifiable zone..................E/4af62fcb-5ce6-4dde-8c8f-c29708de1a4d
     }
 
-    public int searchWordForCount(String word) {
-//begin of modifiable zone(JavaCode)......C/3866f320-3f01-46f5-9cfc-a814243d366c
+    public int getDocumentCount() {
+//begin of modifiable zone(JavaCode)......C/2ed5ee71-395f-4fcb-bde5-e3d58939073d
 
-//end of modifiable zone(JavaCode)........E/3866f320-3f01-46f5-9cfc-a814243d366c
-//begin of modifiable zone(JavaReturned)..C/3866f320-3f01-46f5-9cfc-a814243d366c
-
-//end of modifiable zone(JavaReturned)....E/3866f320-3f01-46f5-9cfc-a814243d366c
+//end of modifiable zone(JavaCode)........E/2ed5ee71-395f-4fcb-bde5-e3d58939073d
+//begin of modifiable zone................T/1cf0fb36-da27-4e0f-bd62-06f85553ad87
+        return reader.numDocs();
+//end of modifiable zone..................E/1cf0fb36-da27-4e0f-bd62-06f85553ad87
     }
 
-    public int searchPhraseForCount(List<String> phrase, int start, int length) {
-//begin of modifiable zone(JavaCode)......C/a001743d-b05c-4bde-bd73-360ea80e12e3
-
-//end of modifiable zone(JavaCode)........E/a001743d-b05c-4bde-bd73-360ea80e12e3
-//begin of modifiable zone(JavaReturned)..C/a001743d-b05c-4bde-bd73-360ea80e12e3
-
-//end of modifiable zone(JavaReturned)....E/a001743d-b05c-4bde-bd73-360ea80e12e3
+    public int searchWordForCount(String word) throws java.io.IOException {
+//begin of modifiable zone................T/e2cda6fd-fe92-4472-b797-6d1e18184e0d
+        Query query = new TermQuery(new Term(IndexBuilder.CONTENT_FIELD_NAME, word.toLowerCase()));
+        TopDocs docs = searcher.search(query, reader.numDocs());
+//end of modifiable zone..................E/e2cda6fd-fe92-4472-b797-6d1e18184e0d
+//begin of modifiable zone................T/4d724e8c-b90d-4ca6-a907-66ff6fbadeaf
+        return docs.scoreDocs.length;
+//end of modifiable zone..................E/4d724e8c-b90d-4ca6-a907-66ff6fbadeaf
     }
 
-    public List<String> getAllWords() {
-//begin of modifiable zone(JavaCode)......C/91af9152-f4be-46ec-9a86-bb49b4ae1aa8
-
-//end of modifiable zone(JavaCode)........E/91af9152-f4be-46ec-9a86-bb49b4ae1aa8
-//begin of modifiable zone(JavaReturned)..C/91af9152-f4be-46ec-9a86-bb49b4ae1aa8
-
-//end of modifiable zone(JavaReturned)....E/91af9152-f4be-46ec-9a86-bb49b4ae1aa8
+    public int searchPhraseForCount(List<String> phrase, int start, int length) throws java.io.IOException {
+//begin of modifiable zone................T/acd4e4d4-866e-4ddf-af8b-099dab63362f
+        PhraseQuery query = new PhraseQuery();
+        for (int i = start; i < (start + length); i++) 
+            query.add(new Term(IndexBuilder.CONTENT_FIELD_NAME, phrase.get(i).toLowerCase()));
+        TopDocs docs = searcher.search(query, reader.numDocs());
+//end of modifiable zone..................E/acd4e4d4-866e-4ddf-af8b-099dab63362f
+//begin of modifiable zone................T/93a0d418-1945-4652-b37f-b7628b7944e1
+        return docs.scoreDocs.length;
+//end of modifiable zone..................E/93a0d418-1945-4652-b37f-b7628b7944e1
     }
 
-    public List<String> getDocumentWordSequence(int id) {
-//begin of modifiable zone(JavaCode)......C/45f7a401-bb25-44b1-aa39-c1b050266158
+    public List<String> getAllWords() throws java.io.IOException {
+//begin of modifiable zone................T/2f3edb8a-3a63-4304-9ddb-34cd01498e7d
+        Vector<String> words = new Vector<String>();
+        Fields fields = MultiFields.getFields(reader);
+        if (fields != null) {
+            Terms terms = fields.terms(IndexBuilder.CONTENT_FIELD_NAME);
+            if (terms != null) {
+                TermsEnum termsEnum = terms.iterator(null);
+                BytesRef bytesRef;
+                while ((bytesRef = termsEnum.next()) != null) {
+                    words.add(bytesRef.utf8ToString());
+                }
+            }            
+        }
+//end of modifiable zone..................E/2f3edb8a-3a63-4304-9ddb-34cd01498e7d
+//begin of modifiable zone................T/72f6dfcc-8093-421b-b37d-7e60eff0fa01
+        return words;
+//end of modifiable zone..................E/72f6dfcc-8093-421b-b37d-7e60eff0fa01
+    }
 
-//end of modifiable zone(JavaCode)........E/45f7a401-bb25-44b1-aa39-c1b050266158
-//begin of modifiable zone(JavaReturned)..C/45f7a401-bb25-44b1-aa39-c1b050266158
-
-//end of modifiable zone(JavaReturned)....E/45f7a401-bb25-44b1-aa39-c1b050266158
+    public List<String> getDocumentWordSequence(int id) throws java.io.IOException {
+//begin of modifiable zone................T/6a08b042-1871-4b88-a8ff-79d8e9d1ab65
+        Vector<String> words = new Vector<String>();
+        Document document = searcher.doc(id);
+        if ((document != null) && (analyzer != null)) {
+            TokenStream stream = document.getField(IndexBuilder.CONTENT_FIELD_NAME).tokenStream(analyzer, null);
+            CharTermAttribute charTermAttribute = stream.addAttribute(CharTermAttribute.class);
+            stream.reset();
+            while (stream.incrementToken()) {
+                words.add(charTermAttribute.toString());
+            }
+            stream.close();            
+        }
+//end of modifiable zone..................E/6a08b042-1871-4b88-a8ff-79d8e9d1ab65
+//begin of modifiable zone................T/b22c2f7c-0813-47ab-9600-cd2260ede7c9
+        return words;
+//end of modifiable zone..................E/b22c2f7c-0813-47ab-9600-cd2260ede7c9
     }
 
 }
---
+++ PSSE/src/org/loccs/index/IndexQuerierTest.java
@@ -0,0 +1,75 @@
+package org.loccs.index;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IndexQuerierTest {
+	
+	private IndexBuilder builder;
+	
+	private IndexQuerier querier;
+
+	@Before
+	public void setUp() throws Exception {
+		builder = new IndexBuilder();
+		builder.prepareIndexDirectory("D:\\Research\\Evaluation\\PSSE\\Data\\Index\\debug");
+		builder.addDirectory("D:\\Research\\Evaluation\\PSSE\\Data\\Documents\\debug");
+		
+		querier = new IndexQuerier("D:\\Research\\Evaluation\\PSSE\\Data\\Index\\debug");
+		querier.setAnalyzer(builder.getAnalyzer());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		builder.close();
+	}
+
+	@Test
+	public void testGetDocumentCount() {
+		assertEquals(3, querier.getDocumentCount());
+	}
+
+	@Test
+	public void testSearchWordForCount() throws IOException {
+		assertEquals(2, querier.searchWordForCount("Holm"));
+	}
+
+	@Test
+	public void testSearchPhraseForCount() throws IOException {
+		Vector<String> phrase = new Vector<String>();
+		phrase.add("New");
+		phrase.add("York");
+		assertEquals(2, querier.searchPhraseForCount(phrase, 0, 2));
+		
+		phrase.clear();
+		phrase.add("Central");
+		phrase.add("Park");
+		phrase.add("West");
+		assertEquals(1, querier.searchPhraseForCount(phrase, 0, 3));
+	}
+
+	@Test
+	public void testGetAllWords() throws IOException {
+		assertEquals(38, querier.getAllWords().size());
+	}
+
+	@Test
+	public void testGetDocumentWordSequence() throws IOException {
+		List<String> words = querier.getDocumentWordSequence(0);
+		assertEquals(19, words.size());
+		assertEquals("holm", words.get(0));
+		assertEquals("new", words.get(4));
+		assertEquals("much", words.get(8));
+		assertEquals("apartment", words.get(14));
+		assertEquals("on", words.get(15));
+		assertEquals("west", words.get(18));
+	}
+
+}
