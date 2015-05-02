a74f0080a9792705aa2e0ce9b488515b82af0868
Longsky <hnlu@sjtu.edu.cn>
2015-05-02 18:24:50 +0800

    Progress in statistics operation.

---
+++ PSSE/src/org/loccs/statistics/PhraseUtils.java
@@ -0,0 +1,24 @@
+package org.loccs.statistics;
+
+import java.util.List;
+
+public class PhraseUtils {
+    public PhraseUtils() {
+//begin of modifiable zone(JavaSuper).....C/a8de0bfd-fc3d-4068-9a8e-4a255b3df7d8
+
+//end of modifiable zone(JavaSuper).......E/a8de0bfd-fc3d-4068-9a8e-4a255b3df7d8
+//begin of modifiable zone(JavaCode)......C/a8de0bfd-fc3d-4068-9a8e-4a255b3df7d8
+
+//end of modifiable zone(JavaCode)........E/a8de0bfd-fc3d-4068-9a8e-4a255b3df7d8
+    }
+
+    public static String toString(List<String> phrase, int start, int length) {
+//begin of modifiable zone(JavaCode)......C/51acbd15-4956-4fba-b1f4-4a33ebe669f3
+    	String phraseString = "";
+//end of modifiable zone(JavaCode)........E/51acbd15-4956-4fba-b1f4-4a33ebe669f3
+//begin of modifiable zone(JavaReturned)..C/51acbd15-4956-4fba-b1f4-4a33ebe669f3
+    	return phraseString;
+//end of modifiable zone(JavaReturned)....E/51acbd15-4956-4fba-b1f4-4a33ebe669f3
+    }
+
+}
